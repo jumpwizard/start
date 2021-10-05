ab25ad7b3cbdb1ae5ec2b0a4b36bacc6f28c9a13
jumpwizard <58380133+jumpwizard@users.noreply.github.com>
2021-10-05 13:04:46 +0800

    Add files via upload

---
+++ week9/ch1/main.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+struct BtNode
+{
+    char ch;
+
+    struct BtNode *LChild;
+
+    struct BtNode *RChild;
+};
+
+//递归函数实现遍历
+void Traversal(struct BtNode *root)
+{
+    //先序遍历
+    if(root==NULL)
+    {
+        return ;
+    }
+
+    //先根
+    printf("%c \n",root->ch);
+
+    //再左
+    Traversal(root->LChild);
+
+    //再右
+    Traversal(root->RChild);
+}
+
+//创造二叉树并遍历
+void Text()
+{
+    //建立节点
+    struct BtNode  NodeA ={'A',NULL,NULL};
+    struct BtNode  NodeB ={'B',NULL,NULL};
+    struct BtNode  NodeC ={'C',NULL,NULL};
+    struct BtNode  NodeD ={'D',NULL,NULL};
+    struct BtNode  NodeE ={'E',NULL,NULL};
+    struct BtNode  NodeF ={'F',NULL,NULL};
+    struct BtNode  NodeG ={'G',NULL,NULL};
+    struct BtNode  NodeH ={'H',NULL,NULL};
+
+    //建立节点之间的关系
+    NodeA.LChild = &NodeB;
+    NodeA.RChild = &NodeF;
+
+    NodeB.RChild = &NodeC;
+
+    NodeC.LChild = &NodeD;
+    NodeC.RChild = &NodeE;
+
+    NodeF.RChild = &NodeG;
+
+    NodeG.LChild = &NodeH;
+
+    //先序遍历
+    Traversal(&NodeA);
+}
+
+int main()
+{
+    Text();
+
+    return 0;
+}
